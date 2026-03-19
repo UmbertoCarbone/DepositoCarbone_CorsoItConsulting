af156612d04084e3202c1d5e275b92f1bd761c60
Arcy.dev <umberto.carb.dev@gmail.com>
2026-03-19 12:58:11 +0100

    function mostra

--- Java19_03/Java19_03_pomeriggio/Funzioni.java
+++ Java19_03/Java19_03_pomeriggio/Funzioni.java
@@ -1,13 +1,23 @@
 package Java19_03.Java19_03_pomeriggio;
 
 public class Funzioni {
-    
+
     static int pippoAddizione(int a, int b) {
         return a + b;
     }
 
+    static void mostra(int numero) {
+        System.out.println("Numero: " + numero);
+    }
+
+    static void mostra(String testo) {
+        System.out.println("Testo: " + testo);
+    }
+
     public static void main(String[] args) {
         System.out.println(pippoAddizione(10, 20));
+        mostra(10);
+        mostra("ciao");
     }
 
 }
