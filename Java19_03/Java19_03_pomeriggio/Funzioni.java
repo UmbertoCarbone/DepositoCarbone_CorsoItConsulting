3cbcb9882ec74bb3a7a972ae92d1c58561f46834
Arcy.dev <umberto.carb.dev@gmail.com>
2026-03-19 12:44:20 +0100

    function

---
+++ Java19_03/Java19_03_pomeriggio/Funzioni.java
@@ -0,0 +1,13 @@
+package Java19_03.Java19_03_pomeriggio;
+
+public class Funzioni {
+    
+    static int pippoAddizione(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(pippoAddizione(10, 20));
+    }
+
+}
