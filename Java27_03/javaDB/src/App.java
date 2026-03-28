4e606e39c39b147cba56cc0d2b4346a920fdb00c
Arcy.dev <umberto.carb.dev@gmail.com>
2026-03-28 12:41:50 +0100

    db-java

---
+++ Java27_03/javaDB/src/App.java
@@ -0,0 +1,21 @@
+import java.sql.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String url = "jdbc:mysql://localhost:3306/sakila";
+        String user = "root";
+        String password = "root";
+/* 
+Connection = oggetto;
+
+*/
+        try {
+            Connection conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connesso al database!");
+            conn.close();
+            System.out.println("Configurazione JDBC ok!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
